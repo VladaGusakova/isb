6d37fb22c1f22fcc2724f62acdff4bff69f02ff1
VladaGusakova <163380239+VladaGusakova@users.noreply.github.com>
2025-05-27 19:45:09 +0500

    Update GenJavaSeq.java
    Add docstring

--- lab_2/GenJavaSeq.java
+++ lab_2/GenJavaSeq.java
@@ -3,7 +3,12 @@ import java.io.IOException;
 import java.util.Random;
 
 public class GenJavaSeq {
-
+    
+    /**
+    * Generates a random bit sequence
+    * @param length Length of the sequence
+    * @return Generated sequence
+    */
     public static String generate_seq(int length) {
         StringBuilder sequence = new StringBuilder(length);
         Random random = new Random();
@@ -14,6 +19,11 @@ public class GenJavaSeq {
         return sequence.toString();
     }
 
+    /**
+    * Saves a sequence to a JSON file
+    * @param sequence The sequence to save
+    * @param filename The file name to save
+    */
     public static void save_seq(String sequence, String filename) {
         try (FileWriter writer = new FileWriter(filename)) {
             writer.write("{\n  \"sequence\": \"" + sequence + "\"\n}");
@@ -22,6 +32,10 @@ public class GenJavaSeq {
         }
     }
 
+    /**
+    * Main method of the program
+    * @param args Command line arguments
+    */
     public static void main(String[] args) {
         String sequence = generate_seq(128);
         save_seq(sequence, "java_seq.json");
